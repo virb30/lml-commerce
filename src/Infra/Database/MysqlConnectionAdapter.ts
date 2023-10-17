0797abb3f22dcbabc2d8fac1b9f70f914e617ef8
Vinicius Boscoa <virb30@gmail.com>
2023-10-17 19:34:12 -0300

    chore: adds dotenv config

--- src/Infra/Database/MysqlConnectionAdapter.ts
+++ src/Infra/Database/MysqlConnectionAdapter.ts
@@ -2,16 +2,16 @@ import { Connection } from "./Connection";
 import mysql from "mysql2";
 
 export class MysqlConnectionAdapter implements Connection {
-  private mysql: any;
-  constructor() {
-    this.mysql = mysql
-      .createConnection("mysql://root:123456@localhost:3306/app");
-  }
-  async query(statement: string, params?: any): Promise<any> {
-    const [result] = await this.mysql.promise().execute(statement, params);
-    return result;
-  }
-  async close(): Promise<void> {
-    await this.mysql.promise().end();
-  }
+    private mysql: any;
+    constructor(connectionString: string) {
+        this.mysql = mysql
+            .createConnection(connectionString);
+    }
+    async query(statement: string, params?: any): Promise<any> {
+        const [result] = await this.mysql.promise().execute(statement, params);
+        return result;
+    }
+    async close(): Promise<void> {
+        await this.mysql.promise().end();
+    }
 }
--- test/MysqlConnectionAdapter.test.ts
+++ test/MysqlConnectionAdapter.test.ts
@@ -1,10 +1,11 @@
 import { MysqlConnectionAdapter } from "../src/Infra/Database/MysqlConnectionAdapter";
+import { getDbConnectionString } from "../src/config";
 
 describe("Database connection test", () => {
-  it("should connection in databse", async () => {
-    const connection = new MysqlConnectionAdapter();
-    const [result] = await connection.query("SELECT 1");
-    await connection.close();
-    expect(result).toEqual({ "1": 1 });
-  });
+    it("should connection in databse", async () => {
+        const connection = new MysqlConnectionAdapter(getDbConnectionString());
+        const [result] = await connection.query("SELECT 1");
+        await connection.close();
+        expect(result).toEqual({ "1": 1 });
+    });
 });
