d397c0f7788fb7fb2aed77553ac0f15f301f697c
Luca Pereira <43727657+lucaspereira-dev@users.noreply.github.com>
2023-10-17 21:24:05 -0300

    Update test/MysqlConnectionAdapter.test.ts
    Co-authored-by: Vinícius Bôscoa <virb30@gmail.com>

--- test/MysqlConnectionAdapter.test.ts
+++ test/MysqlConnectionAdapter.test.ts
@@ -1,8 +1,8 @@
 import { MysqlConnectionAdapter } from "../src/Infra/Database/MysqlConnectionAdapter";
 import { getDbConnectionString } from "../src/config";
 
-describe("Database connection test", () => {
-    it("should connection in databse", async () => {
+describe("Database connection tests", () => {
+    it("should connect to a database", async () => {
         const connection = new MysqlConnectionAdapter(getDbConnectionString());
         const [result] = await connection.query("SELECT 1");
         await connection.close();
