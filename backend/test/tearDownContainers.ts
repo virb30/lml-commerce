ce904a6d35e7a668b4cda0fdebe50336984c433b
Vinicius Boscoa <virb30@gmail.com>
2024-08-30 22:50:53 -0300

    test: organize testcontainer setup

--- backend/test/tearDownContainers.ts
+++ backend/test/tearDownContainers.ts
@@ -5,7 +5,8 @@ export default async function tearDownContainers() {
   const containers = await containerRuntimeClient.container.list();
   for (const containerInfo of containers) {
     const container = containerRuntimeClient.container.getById(containerInfo.Id);
-    await container.stop();
-    await container.remove();
+    await container.remove({
+      force: true,
+    });
   }
 }
